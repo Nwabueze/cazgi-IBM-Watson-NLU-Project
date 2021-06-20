e03db4411f6238e121ad2702464ec21a064a4a6b
Nwabueze <snlnwabueze@gmail.com>
2021-06-20 23:27:02 +0000

    temp changes --2m

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -46,18 +46,23 @@ class App extends React.Component {
     ret = axios.get(url);
     ret.then((response)=>{
 
-      //Include code here to check the sentiment and fomrat the data accordingly
-
+      //Include code here to check the sentiment and format the data accordingly
+      console.log(response);
       this.setState({sentimentOutput:response.data});
       let output = response.data;
+      console.log(response.data);
+      
       if(response.data === "positive") {
         output = <div style={{color:"green",fontSize:20}}>{response.data}</div>
       } else if (response.data === "negative"){
         output = <div style={{color:"red",fontSize:20}}>{response.data}</div>
       } else {
-        output = <div style={{color:"orange",fontSize:20}}>{response.data}</div>
+        output = <div style={{color:"yellow",fontSize:20}}>{response.data}</div>
       }
+      
       this.setState({sentimentOutput:output});
+      
+      this.setState({sentimentOutput:"positive"});
     });
   }
 
